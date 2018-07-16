5b5d93a02afba14aff8887f6c62f76f7eb853309
GurraB <gustaf.t.bohlin@gmail.com>
2018-07-16 15:20:35 -0400

    Added Decision tree classifier

--- Pulse_estimation/Analysis/Analysis.py
+++ Pulse_estimation/Analysis/Analysis.py
@@ -80,9 +80,141 @@ def scatter_pulse_score(data):
     plt.scatter(scores, pulse)
     plt.show()
 
+
+def scatter_genre_scores(data):
+    names = stimuli_id.keys()
+    names = sorted(names, key=lambda name: stimuli_id[name])
+    scores = []
+    for name in names:
+        scores.append(data.get_column_for_video('One a scale of 1 to 5, how would you rate the video?', name))
+
+    scores_genre = []
+    for i in range(5):
+        scores_genre.append([])
+        scores_genre[i].extend(scores[i * 3])
+        scores_genre[i].extend(scores[(i * 3)+ 1])
+        scores_genre[i].extend(scores[(i * 3) + 2])
+
+    scores_amount_genre = []
+    for i in range(5):
+        scores_amount_genre.append([])
+        for j in range(5):
+            scores_amount_genre[i].append(scores_genre[i].count((j + 1))**2.5)
+
+    print(scores_amount_genre)
+    genres = []
+    #for i in range(len(intended_emotions)):
+    #    genres.append([i + 1 for j in range(len(scores_genre[0]))])
+
+    genres = [[i + 1 for j in range(5)] for i in range(5)]
+    scores = [[j + 1 for j in range(5)] for i in range(5)]
+    print genres
+    plt.figure(1)
+    plt.scatter(genres, scores, s=scores_amount_genre)
+    plt.axes().set_title('Is there a correleation between the average score for a video and the rating given?')
+    plt.axes().set_ylabel('Score')
+    plt.axes().set_xticklabels(intended_emotions)
+    plt.axes().set_xticks([1, 2, 3, 4, 5])
+    plt.show()
+
+
+def box_joy_video(data):
+    names = stimuli_id.keys()
+    names = sorted(names, key=lambda name: stimuli_id[name])
+
+    joy = []
+    for i in range(len(names)):
+        joy.append(data.get_column_for_video('Joy_average', names[i]))
+
+    plt.figure(1)
+    plt.boxplot(joy)
+    plt.axes().set_xticklabels(names)
+    plt.show()
+
+
+def hist_emotion_facial_expressions(data):
+    names = stimuli_id.keys()
+    names = sorted(names, key=lambda name: stimuli_id[name])
+
+    facial_expressions_for_Joy = []
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Joy.append([])
+        facial_expressions_for_Joy[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Noah'))
+        facial_expressions_for_Joy[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Cat'))
+        facial_expressions_for_Joy[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Despicable'))
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Joy[i] = sum(facial_expressions_for_Joy[i]) / len(facial_expressions_for_Joy[i])
+
+    facial_expressions_for_Sadness = []
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Sadness.append([])
+        facial_expressions_for_Sadness[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Dog'))
+        facial_expressions_for_Sadness[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Forest'))
+        facial_expressions_for_Sadness[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Interstellar'))
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Sadness[i] = sum(facial_expressions_for_Sadness[i]) / len(facial_expressions_for_Sadness[i])
+
+    facial_expressions_for_Anger = []
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Anger.append([])
+        facial_expressions_for_Anger[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Wheelchair'))
+        facial_expressions_for_Anger[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Help'))
+        facial_expressions_for_Anger[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Amish'))
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Anger[i] = sum(facial_expressions_for_Anger[i]) / len(facial_expressions_for_Anger[i])
+
+    facial_expressions_for_Fear = []
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Fear.append([])
+        facial_expressions_for_Fear[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Iguana'))
+        facial_expressions_for_Fear[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Shining'))
+        facial_expressions_for_Fear[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Dino'))
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Fear[i] = sum(facial_expressions_for_Fear[i]) / len(facial_expressions_for_Fear[i])
+
+    facial_expressions_for_Surprise = []
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Surprise.append([])
+        facial_expressions_for_Surprise[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Magic Bird'))
+        facial_expressions_for_Surprise[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Flying'))
+        facial_expressions_for_Surprise[i].extend(data.get_column_for_video(intended_emotions[i] + '_average', 'Cocaine'))
+    for i in range(len(intended_emotions)):
+        facial_expressions_for_Surprise[i] = sum(facial_expressions_for_Surprise[i]) / len(facial_expressions_for_Surprise[i])
+
+    plt.figure(1)
+
+    plt.subplot(1, 5, 1)
+    plt.bar(intended_emotions, facial_expressions_for_Joy)
+    plt.title('Joyful video')
+
+    plt.subplot(1, 5, 2)
+    plt.bar(intended_emotions, facial_expressions_for_Sadness)
+    plt.title('Sad video')
+
+    plt.subplot(1, 5, 3)
+    plt.bar(intended_emotions, facial_expressions_for_Anger)
+    plt.title('Anger video')
+
+    plt.subplot(1, 5, 4)
+    plt.bar(intended_emotions, facial_expressions_for_Fear)
+    plt.title('Fearful video')
+
+    plt.subplot(1, 5, 5)
+    plt.bar(intended_emotions, facial_expressions_for_Surprise)
+    plt.title('Surprising video')
+
+    plt.show()
+
+
 def Analysis():
     data = load_data.Data('/home/gustaf/Downloads/data/final/')
-
+    bar_diagram_video_rating(data)
+    box_pulse_video(data)
+    box_pulse_score(data)
+    scatter_pulse_score(data)
+    scatter_genre_scores(data)
+    box_joy_video(data)
+    hist_emotion_facial_expressions(data)
 
 
 if __name__ == '__main__':
---
+++ Pulse_estimation/ML_models/Decision_tree.py
@@ -0,0 +1,52 @@
+from sklearn import tree
+import os
+import pandas as pd
+import numpy as np
+import graphviz
+from sklearn.model_selection import cross_val_score
+
+wanted_columns = ['Pulse_derivative_min', 'Pulse_derivative_max', 'Pulse_derivative_average',
+                  'Engagement_min', 'Engagement_max', 'Engagement_average',
+                  'Attention_min', 'Attention_max', 'Attention_average',
+                  'Valence_min', 'Valence_max', 'Valence_average',
+                  'Anger_min', 'Anger_max', 'Anger_average',
+                  'Sadness_min', 'Sadness_max', 'Sadness_average',
+                  'Disgust_min', 'Disgust_max', 'Disgust_average',
+                  'Joy_min', 'Joy_max', 'Joy_average',
+                  'Surprise_min', 'Surprise_max', 'Surprise_average',
+                  'Fear_min', 'Fear_max', 'Fear_average',
+                  'Contempt_min', 'Contempt_max', 'Contempt_average']
+
+
+def load_data(working_directory):
+    os.chdir(working_directory)
+    df = pd.read_csv('master.csv')
+    data_X = []
+    for column in wanted_columns:
+        data_X.append((df[column]))
+    data_X = np.transpose(data_X)
+    data_y = list(df['One a scale of 1 to 5, how would you rate the video?'])
+
+    return data_X, data_y
+
+
+def plot_tree(clf):
+    dot_data = tree.export_graphviz(clf, out_file=None)
+    graph = graphviz.Source(dot_data)
+    graph.render("rating")
+
+
+def cross_validation(clf, data_X, data_y, folds):
+    scores = cross_val_score(clf, data_X, data_y, cv=folds)
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+def main():
+    data_X, data_y = load_data('/home/gustaf/Downloads/data/final/')
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(data_X, data_y)
+    cross_validation(clf, data_X, data_y, 5)
+    #plot_tree(clf)
+
+
+if __name__ == '__main__':
+    main()
